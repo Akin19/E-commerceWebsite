df603d02ee3f4c2285753edb122ab0a64512bdb7
Akin19 <asadujjamanakin2@gmail.com>
2025-01-05 22:36:25 +0600

    final commit

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,7 +3,14 @@ import { assets } from "../assets/admin_assets/assets";
 const Navbar = ({ setToken }) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
-      <img className="w-[max-(10%,80px)]" src={assets.logo} alt="" />
+      <div className="h-[70px] w-[80px]">
+        <img
+          className="w-full h-full object-contain"
+          src={assets.logo}
+          alt=""
+        />
+      </div>
+
       <button
         onClick={() => setToken("")}
         className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full"
